bd25ffb4b011d6fdf1887ca7e3ed0d3aaedb2aee
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-08-13 12:41:16 +0300

    change to Junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,18 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import java.util.concurrent.Callable;
+import static org.junit.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
     public void shouldAmount1000() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -19,7 +20,7 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -27,7 +28,7 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(800);
         int expected = 200;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -35,6 +36,6 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1100);
         int expected = 900;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
 }
